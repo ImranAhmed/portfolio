303e7a267385620721a224cbff472ee2ca668ac5
Imran Ahmed <imran.ahmed@qbridgeplatform.com>
2020-07-25 22:53:52 +0100

    Updated bio

--- components/bio/bio.tsx
+++ components/bio/bio.tsx
@@ -5,14 +5,14 @@ const Bio: React.FC = () => (
     <div className="body2">
         <div className={styles.summary}>
             I am a <span className={styles.highlight}>software engineer</span> with over{' '}
-            <span className={styles.highlight}>twenty</span> years development experience acorss bulge-bracket{' '}
+            <span className={styles.highlight}>twenty</span> years development experience across bulge-bracket{' '}
             <span className={styles.highlight}>investment banks</span> and{' '}
             <span className={styles.highlight}>start-ups</span>. I am comfortable working across the
             <span className={styles.highlight}>&nbsp;full stack</span> and also{' '}
             <span className={styles.highlight}>leading</span> small to medium sized{' '}
             <span className={styles.highlight}>high-performance&nbsp;</span>
             development teams. I love taking <span className={styles.highlight}>ownership</span> of the entire product
-            suite and also representation to senior <span className={styles.highlight}>stakeholders</span>, and working
+            suite including representation to senior <span className={styles.highlight}>stakeholders</span>, and working
             directly with
             <span className={styles.highlight}>&nbsp;entrepreneurs</span>.
         </div>
