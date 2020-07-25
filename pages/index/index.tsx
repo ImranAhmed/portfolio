1521385425df24e83cd5fee4a04b2e1d403a89e6
Imran Ahmed <imran.ahmed@qbridgeplatform.com>
2020-07-25 16:30:37 +0100

    Creating portfolio sute

--- pages/index/index.tsx
+++ pages/index/index.tsx
@@ -5,7 +5,7 @@ import styles from './index.module.scss';
 const Home: NextPage = () => {
     return (
         <div className={styles.container}>
-            <h1>Imran Ahmed</h1>
+            <h1 className="headline1">Imran Ahmed</h1>
             <Button text="click" />
         </div>
     );
