64e4bd06cba37b190b99b682daf8e45bfcba9861
Imran Ahmed <imran.ahmed@qbridgeplatform.com>
2020-07-26 13:51:43 +0100

    Setting up story book via cli

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,6 +1,3 @@
-const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-
 module.exports = ({ config }) => {
     config.module.rules.push({
         test: /\.(ts|tsx)$/,
@@ -9,39 +6,15 @@ module.exports = ({ config }) => {
             presets: [require.resolve('babel-preset-react-app')],
         },
     });
-    config.module.rules.push({
-        test: /\.(sass|scss)$/,
-        use: [
-            'style-loader',
-            'css-loader',
-            {
-                loader: 'sass-loader',
-                options: {
-                    sassOptions: {
-                        indentWidth: 4,
-                        includePaths: [path.resolve(__dirname, 'node_modules')],
-                    },
-                },
-            },
-        ],
-    });
-    config.module.rules.push({
-        test: /\.(gif|png|jpe?g|svg)$/i,
-        use: [
-            'file-loader',
-            {
-                loader: 'image-webpack-loader',
-                options: {
-                    disable: true,
-                },
-            },
-        ],
-    });
+
     config.resolve.extensions.push('.ts', '.tsx');
-    config.plugins.push(
-        new CopyWebpackPlugin({
-            patterns: [{ from: 'public/images', to: 'images' }],
-        }),
-    );
+
+    config.module.rules.push({
+        test: /\.(svg|ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani)(\?.*)?$/,
+        loader: require.resolve('file-loader'),
+        query: {
+            name: 'static/media/[name].[hash:8].[ext]',
+        },
+    });
     return config;
 };
