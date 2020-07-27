62cfa2fb0d40cc1548add71f86e805a9bd93a6c5
Imran Ahmed <imran.ahmed@qbridgeplatform.com>
2020-07-27 20:24:31 +0100

    Moved head to app component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,19 @@
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 import { ReactElement } from 'react';
 import '../styles/global.scss';
 
 const App = ({ Component, pageProps }: AppProps): ReactElement => {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Head>
+                <title>Portfolio: Imran Ahmed</title>
+                <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 };
 
 export default App;
