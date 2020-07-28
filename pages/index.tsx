d859f5ac1f1cbad7a66fdb470dd01df2b5ed376a
Imran Ahmed <imran.ahmed@qbridgeplatform.com>
2020-07-28 08:00:21 +0100

    Removed redundent markup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,6 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import { NextPage } from 'next';
 import { Home } from '../containers';
 
-const name = 'Imran Ahmed';
+const Index: NextPage = () => <Home name="Imran Ahmed" />;
 
-const Index: NextPage = () => {
-    return (
-        <>
-            <Home name={name} />
-        </>
-    );
-};
 export default Index;
