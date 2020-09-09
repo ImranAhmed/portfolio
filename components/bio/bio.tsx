4be31b4c1776a407624f5c65d78f0bfb55f25cbc
Imran Ahmed <imran.ahmed@qbridgeplatform.com>
2020-09-09 08:55:46 +0100

    Updated bio data

--- components/bio/bio.tsx
+++ components/bio/bio.tsx
@@ -9,10 +9,12 @@ const Bio: React.FC = () => (
             I am a <span className={styles.highlight}>software engineer</span> with over{' '}
             <span className={styles.highlight}>twenty</span> years development experience across{' '}
             <span className={styles.highlight}>investment banking</span> and{' '}
-            <span className={styles.highlight}>start-ups</span>. I am highly experienced on the{' '}
-            <span className={styles.highlight}>front-end</span> and equally at home working across the
-            <span className={styles.highlight}>&nbsp;full stack</span> (including infrastructure). I enjoy team leading
-            and being part of small to medium sized <span className={styles.highlight}>high-performance&nbsp;</span>
+            <span className={styles.highlight}>start-ups</span>. During my career I have worked on unique applications{' '}
+            that have managed <span className={styles.highlight}>trillions</span> of dollars of{' '}
+            <span className={styles.highlight}>capital</span>. I am highly experienced on the{' '}
+            <span className={styles.highlight}>front-end</span> and equally at home working across the{' '}
+            <span className={styles.highlight}>full stack</span> (including infrastructure). I enjoy team leading and
+            being part of small to medium sized <span className={styles.highlight}>high-performance&nbsp;</span>
             development teams. I love taking <span className={styles.highlight}>ownership</span> of the entire product
             suite including representation to senior <span className={styles.highlight}>stakeholders</span> and working
             directly with
